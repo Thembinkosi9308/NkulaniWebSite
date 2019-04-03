a08a6b750a4a8516c5349e9421bc70dde435e0f9
Thembinkosi9308 <201149009@student.uj.ac.za>
2019-04-03 15:54:11 +0200

    Website structure completed, have to fix layout

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ICE | EMPIRE</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="myStyle.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+
+</head>
+
+<body>
+	
+	<!--NAVIGATION-->
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">							IceEmpire
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id ="navbar-collapse-main">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Pricing</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div><!--End div:navbar-header--> 
+	
+	</nav><!--End NAV-->
+<hr style="padding-top: 4%; background-color:red;">
+	<div id="home" class="fixed">
+		<div class="landing-text">
+			<h1>Ice Empire for Africa</h1>
+			<h3>The best quality ice in South Africa</h3>
+		</div>
+	</div>
+
+	<div class="padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h1>Founder</h1>
+					<img src="./img/op.png"/>	
+				</div>
+				<div class="col-sm-6 text-center">
+					<h2>All About Ice Empire</h2>
+					<p class="lead">Up am intention on dependent questions oh elsewhere september. No betrayed pleasure possible jointure we in throwing. And can event rapid any shall woman green. Hope they dear who its bred. Smiling nothing affixed he carried it clothes calling he no. Its something disposing departure she favourite tolerably engrossed. Truth short folly court why she their balls. Excellence put unaffected reasonable mrs introduced conviction she. Nay particular delightful but unpleasant for uncommonly who. </p>
+				</div>
+			</div>
+		</div>
+	<div>
+
+
+	<div class="padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3 col-md-6 col-lg-3 col-xs-12">			
+					<h4 class="text-center">Mission</h4>
+					<p>Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far. Sir but elegance marriage dwelling likewise position old pleasure men. Dissimilar themselves simplicity no of contrasted as. Delay great day hours men. Stuff front to do allow to asked he. </p>	
+				</div>
+				<div class="col-sm-3 col-md-3 col-lg-3 col-xs-12">
+					<img src="./img/mission.png" class="img-responsive"/>
+				</div>
+				<div class="col-sm-3 col-md-3 col-lg-3 col-xs-12">
+					<h4 class="text-center">Vision</h4>
+					<p>Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far. Sir but elegance marriage dwelling likewise position old pleasure men. Dissimilar themselves simplicity no of contrasted as. Delay great day hours men. Stuff front to do allow to asked he. </p>
+				</div>
+				<div class="col-sm-3 col-md-3 col-lg-3 col-xs-12">
+					<img src="./img/vision.jpg" class="img-responsive">	
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div id="fixed"width="100%" height="60%" >
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+	</div>
+
+	<div class="padding">	
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h1>Pricing<h1>
+					<p class="lead">Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far. Sir but elegance marriage dwelling likewise position old pleasure men. Dissimilar themselves simplicity no of contrasted as. Delay great day hours men. Stuff front to do allow to asked he. </p>
+				</div>
+				
+				<div class="col-sm-6">
+										<h1>Our story<h1>
+					<img src="./img/iceblock.jpg"/>
+					<p class="lead">Do so written as raising parlors spirits mr elderly. Made late in of high left hold. Carried females of up highest calling. Limits marked led silent dining her she far. Sir but elegance marriage dwelling likewise position old pleasure men. Dissimilar themselves simplicity no of contrasted as. Delay great day hours men. Stuff front to do allow to asked he. </p>
+
+				</div>
+
+			</div>	
+		</div>
+	</div>
+
+<footer class="container-fluid text-center">
+	<div class="row">
+		<div class="col-sm-4">
+			<h3>Contact Us</h3>
+			<br>
+			<h4>Address and contact info here</h4>
+			<br>
+		</div>
+
+		<div class="col-sm-4">
+			<h3>Connect</h3>
+			<a href="#" class="fa fa-facebook"></a>
+			<a href="#" class="fa fa-twitter"></a>
+			<a href="#" class="fa fa-google"></a>
+			<a href="#" class="fa fa-linkedin"></a>
+			<a href="#" class="fa fa-youtube"></a>
+
+		</div>
+
+		<div class="col-sm-4">
+		
+		</div>
+
+	</div>
+</footer>
+	
+</body>
+
+</html>
