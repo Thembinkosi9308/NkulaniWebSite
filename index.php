aac2444fd0944d6662b1c0830cf58fd243b8ba52
Thembinkosi9308 <201149009@student.uj.ac.za>
2019-04-17 09:57:36 +0200

    Footer issue has been solved, problem with navigation in terms of the toggle functionality. Do not know what's wrong but have to change it to check if whether I have done it correctly

--- index.php
+++ index.php
@@ -18,17 +18,17 @@
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main" aria-expanded="false">
+        	 <span class="sr-only">Toggle navigation</span>
+         	<span class="icon-bar"></span>
+         	<span class="icon-bar"></span>
+         	<span class="icon-bar"></span>
+      </button>
 				<a class="navbar-brand" href="#">							IceEmpire
 				</a>
 			</div>
 			<div class="collapse navbar-collapse" id ="navbar-collapse-main">
-				<ul class="nav navbar-nav navbar-right">
+				<ul class="nav navbar-nav navbar-righ mr-auto">
 					<li class="active"><a href="#">Home</a></li>
 					<li><a href="#about">About</a></li>
 					<li><a href="#pricing">Pricing</a></li>
@@ -124,48 +124,100 @@
 		</div>
 	</div>
 
-<footer class="container-fluid text-center" id="contact" height="1">
-	<div class="row">
-		<div class="col-sm-4">
-			<h2>Contact Us</h2>
-			<br>
-			<p align="left">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d894.5907578024674!2d27.877376929162896!3d-26.24987869616183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e95a6df56aa5cb1%3A0xdff19a5ffe0b4353!2s664+Mlangeni+St%2C+Jabavu%2C+Soweto%2C+1809!5e0!3m2!1saf!2sza!4v1554363466564!5m2!1saf!2sza" width="400" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-			</p>
 
-			<br>
-			
-	<!-- 	<p><h4><strong>Address:</strong></h4>
-			664A Mlangeni Street<br>
-			White City Jabavu<br>
-			P.O. Kwa-Xuma<br>
-			1868
-		</p>
-			<br>
-			<p>
-				<strong>Phone:</strong>
-				<br>
-				(+27) 78 921 9364
-			</p>  -->
-		
-		</div>
+<footer class="page-footer font-small unique-color-dark" id="contact">
 
-		<div class="col-sm-8">
-			<h2>Connect</h2>
-			<a href="#" class="fa fa-facebook"></a>
-			<a href="#" class="fa fa-twitter"></a>
-			<a href="#" class="fa fa-google"></a>
-			<a href="#" class="fa fa-linkedin"></a>
-			<a href="#" class="fa fa-youtube"></a>
+    <div style="background-color: #6351ce;">
+      <div class="container">
 
-		</div>
+        <!-- Grid row-->
+        <div class="row py-4 d-flex align-items-center">
 
-<!-- 		<div class="col-sm-4">
-		
-		</div>
- -->
-	</div>
-</footer>
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h5 class="mb-0">Get connected with us on social networks!</h5 >
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fa fa-facebook"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fa fa-twitter"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fa fa-google"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fa fa-linkedin"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fa fa-instagram"> </i>
+            </a>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+
+      <!-- Grid row -->
+      <div class="row mt-3">
+
+        <!-- Grid column -->
+        <div class="col-md-8 col-lg-9 col-xl-3 mx-auto mb-md mb-4">
+
+          <p>
+          	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3578.36314590342!2d27.875735414531796!3d-26.249874972205596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e95a6df56aa5cb1%3A0xdff19a5ffe0b4353!2s664+Mlangeni+St%2C+Jabavu%2C+Soweto%2C+1809!5e0!3m2!1saf!2sza!4v1555487368832!5m2!1saf!2sza" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </p>
+
+        </div>
+        
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <div class="align-items-center">
+          <p>
+            <i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> info@example.com</p>
+          <p>
+            <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p>
+            <i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+  </footer>
+
+
+
+
 	
 </body>
 
